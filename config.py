30ef49906c5c9644cc3e7decc941821c3ea3e07a
MR-SYSTEM-0 <mrhari659@gmail.com>
2025-12-27 18:33:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/NoxxOP/ShrutixMusic",
+    "https://github.com/MR-SYSTEM-0/SIYA-MUZIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
